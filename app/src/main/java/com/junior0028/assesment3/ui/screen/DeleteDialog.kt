de13fb85c825d956ff6b629efce4d410590c84a2
deklyjunior <juniordekly@gmail.com>
2025-06-13 19:33:30 +0700

    menambahkan delete dialog

---
+++ app/src/main/java/com/junior0028/assesment3/ui/screen/DeleteDialog.kt
@@ -0,0 +1,36 @@
+package com.junior0028.assesment3.ui.screen
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.junior0028.assesment3.R
+import com.junior0028.assesment3.model.Menu
+
+@Composable
+fun DeleteDialog(
+    menu: Menu,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = stringResource(R.string.hapus_hewan_title))
+        },
+        text = {
+            Text(text = stringResource(R.string.hapus_hewan_body, menu.nama))
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = stringResource(R.string.hapus))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = stringResource(R.string.batal))
+            }
+        }
+    )
+}
